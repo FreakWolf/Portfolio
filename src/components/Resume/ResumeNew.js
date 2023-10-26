bdefe4a7d6af035d78012ddb3c82eb8c35552184
Rohit Singh <100259763+FreakWolf@users.noreply.github.com>
2023-10-26 15:53:52 +0530

    Updated Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://drive.google.com/file/d/1En5baxJQ4NUeIObzfl4NEYYvyapoQr0w/view?usp=sharing";
+  "https://github.com/FreakWolf/Portfolio/blob/master/src/Assets/Rohit_Singh_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
